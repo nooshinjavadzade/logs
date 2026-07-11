dbcf002ebcf422ae7aa99e78e4294445c84dad33
nooshinjavadzade <javadzadenooshin@gmail.com>
2026-07-11 16:05:24 +0330

    optimizing

--- include/Analyzer.h
+++ include/Analyzer.h
@@ -4,11 +4,14 @@
 #include <unordered_set>
 #include <string>
 #include <vector>
+#include <cstdint> 
 
 using namespace std;
 
 class Analyzer {
 public:
+    Analyzer();
+
     void processEntry(const LogEntry& entry);
     void processInvalidLine();
     
@@ -17,12 +20,12 @@ public:
     void printSuspiciousActivity() const;
 
 private:
-    long long totalRequests = 0;
-    long long invalidLines = 0;
-    long long errorRequests = 0; 
-    
-    unordered_set<string> uniqueIPs;
-    unordered_map<string, long long> endpointCounts;
-    long long hourlyRequests[24] = {0};
-    unordered_map<string, long long> suspiciousIPs;
+    uint64_t totalRequests = 0;
+    uint64_t invalidLines = 0;
+    uint64_t errorRequests = 0; 
+    unordered_set<uint32_t> uniqueIPs;
+    unordered_map<uint32_t, uint64_t> suspiciousIPs;
+    unordered_map<std::string, uint64_t> endpointCounts;
+    uint64_t hourlyRequests[24] = {0};
+    static uint32_t parseIpToUint32(const std::string& ipStr);
 };
